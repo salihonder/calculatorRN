f81ab72992d85e589f2f7c8d8723c97a6c257882
Manal <herotan505@gmail.com>
2023-03-20 00:08:24 +0400

    add header

--- src/components/Manal.js
+++ src/components/Manal.js
@@ -1,7 +1,7 @@
 import {Text, StyleSheet} from 'react-native';
 
 const Manal =() => {
-    return <Text style={Style.text}>Manal</Text>
+    return <Text style={Style.text}>Manal RNP-10</Text>
 }
 
 const Style = StyleSheet.create({
